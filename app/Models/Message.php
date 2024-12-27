f831f0fb6c71c414ea37ee1884f43d622bdc554f
iniLabs-chayan <chayanroy@inilabs.net>
2024-12-27 17:34:17 +0600

    databae setup done

---
+++ app/Models/Message.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Message extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['attachments', 'content', 'user_id', 'is_typing'];
+
+    protected $casts = [
+        'attachments' => 'array'
+    ];
+
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
